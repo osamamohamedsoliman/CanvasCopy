07dc5621a1f5529385a3cc973ef9602571f05534
Osama Soliman <osama.soliman@sjsu.edu>
2020-12-07 13:53:48 -0800

    Canvas backend copy with subject and topic classes complete

---
+++ src/main/java/io/CanvasCopy/SpringBoot/CanvasCopyApp.java
@@ -0,0 +1,15 @@
+package io.CanvasCopy.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasCopyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasCopyApp.class,args );
+		
+		
+	}
+
+}
